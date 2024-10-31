ccb2483fc5a05e914ab581f82c3e6b89b1fab28e
Giu <alcantaragiubs@gmail.com>
2024-10-31 11:25:32 -0300

    Criando o menu inicial

---
+++ Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import System.IO (hFlush, stdout)
+import Cadastro (iniciarCadastro)
+import Login (login)
+
+main :: IO ()
+main = do
+    putStrLn "Menu Principal:"
+    putStrLn "1. Cadastrar Paciente ou Médico"
+    putStrLn "2. Login"
+    putStrLn "0. Sair"
+    putStr "Digite sua escolha: "
+    hFlush stdout
+    escolha <- getLine
+    case escolha of
+        "1" -> iniciarCadastro >> main  -- Chama o submenu de cadastro
+        "2" -> login >> main  -- Chama a função de login
+        "0" -> putStrLn "Encerrando o sistema..."
+        _   -> do
+            putStrLn "Opção inválida. Tente novamente."
+            main
+

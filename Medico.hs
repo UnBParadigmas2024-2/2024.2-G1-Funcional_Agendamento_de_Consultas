4f3aac8f021053d32fd67be5cfae7e6ba2b346fb
Breno Henrique <breno.hs@outlook.com>
2024-11-02 00:14:43 -0300

    Adicionando submenu de Medicos

---
+++ Medico.hs
@@ -0,0 +1,27 @@
+module Medico (submenuMedico) where
+
+import ApagarConta (apagarConta)
+import System.IO (hFlush, stdout)
+
+submenuMedico :: String -> IO ()
+submenuMedico crm = do
+    putStrLn "\nMenu do Médico"
+    putStrLn "1. Visualizar meus dados"
+    putStrLn "2. Apagar minha conta"
+    putStrLn "3. Visualizar minha agenda (consultas agendadas e horários disponíveis)"
+    putStrLn "4. Desmarcar consultas"
+    putStrLn "5. Sair do sistema"
+    putStr "Escolha uma opção: "
+    hFlush stdout
+    escolha <- getLine
+    case escolha of
+        "1" -> putStrLn "Funcionalidade de visualização de dados do médico ainda não implementada."
+        "2" -> do
+            apagarConta crm  -- Chama a função para apagar a conta
+            putStrLn "Conta apagada com sucesso!"
+        "3" -> putStrLn "Funcionalidade de visualização de agenda ainda não implementada."
+        "4" -> putStrLn "Funcionalidade de desmarcação de consultas ainda não implementada."
+        "5" -> putStrLn "Saindo do sistema..."
+        _   -> do
+            putStrLn "Opção inválida. Tente novamente."
+            submenuMedico crm

6066259e155d7a65fa6c52f29f6451ea18e039d4
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-11-07 22:25:37 -0300

    Criando funcao para atualizar os dados do medico
    Co-authored-by: augustocrmg <augusto.duraes33@gmail.com>

--- code/ExibicaoDados.hs
+++ code/ExibicaoDados.hs
@@ -1,7 +1,7 @@
-module ExibicaoDados (exibirDados, exibirDadosPacientes, exibirDadosMedicos) where
+module ExibicaoDados (exibirDados, exibirDadosPacientes, exibirDadosMedicos, atualizarDadosMedico, atualizarDadosPaciente) where
 
 import System.IO (hFlush, stdout, appendFile, writeFile)
-import System.Directory (doesFileExist)
+import System.Directory (doesFileExist, removeFile, renameFile)
 import Data.List (isInfixOf)
 
 -- Função para exibir o conteúdo dos arquivos filtrando pelas linhas que contêm a string CRM
@@ -34,4 +34,33 @@ exibirDadosMedicos key = do
             let linhasComCRM = filter (isInfixOf key) (lines conteudo)
             mapM_ putStrLn linhasComCRM
             putStrLn "\n"
+        else putStrLn "Arquivo de médicos não encontrado."
+
+-- Função para atualizar dados de um médico
+atualizarDadosMedico :: String -> IO ()
+atualizarDadosMedico key = do
+    putStrLn "\nAtualizando dados do médico:"
+    putStrLn "Digite o novo nome do médico:"
+    nomeMedico <- getLine
+    putStrLn "Digite a nova senha:"
+    senha <- getLine
+    putStrLn "Digite a nova especialidade:"
+    especialidade <- getLine
+
+    -- Formata os novos dados do médico
+    let novosDados = nomeMedico ++ "|" ++ senha ++ "|" ++ key ++ "|" ++ especialidade
+
+    -- Verifica se o arquivo de médicos existe
+    arquivoExistente <- doesFileExist "medicos.txt"
+    if arquivoExistente
+        then do
+            -- Lê o conteúdo do arquivo e atualiza a linha correspondente
+            conteudo <- readFile "medicos.txt"
+            let linhas = lines conteudo
+            let linhasAtualizadas = map (\linha -> if key `isInfixOf` linha then novosDados else linha) linhas
+            -- Salva as mudanças reescrevendo o arquivo
+            writeFile "medicos_temp.txt" (unlines linhasAtualizadas)
+            removeFile "medicos.txt"
+            renameFile "medicos_temp.txt" "medicos.txt"
+            putStrLn "Dados do médico atualizados com sucesso.\n"
         else putStrLn "Arquivo de médicos não encontrado."

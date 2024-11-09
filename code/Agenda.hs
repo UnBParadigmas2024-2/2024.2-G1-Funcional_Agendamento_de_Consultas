1ed4743a2bd93a37c1c5a845ec29f8aeacee9b62
edilbertocantuaria <edilbertounbfga@gmail.com>
2024-11-09 13:59:43 -0300

    Ajustando alguns trechos do código

--- code/Agenda.hs
+++ code/Agenda.hs
@@ -1,10 +1,16 @@
 module Agenda (buscaConsultas) where
 
-import System.IO (hFlush, stdout, readFile, writeFile)
+import System.IO (hFlush, stdout, readFile)
 import System.Directory (doesFileExist)
-import Text.Regex (mkRegex, matchRegex)
-import Data.List (isInfixOf, partition)
-import Util(validadorCpf, validadorData, validadorFormatoCRM)
+import Data.List (isInfixOf, partition, nub, (\\))
+import Util (validadorCpf, validadorData, validadorFormatoCRM)
+
+-- Lista de horários padrão (08:00 - 18:00) com intervalos de 30 minutos
+horariosPadrao :: [String]
+horariosPadrao = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", 
+                  "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", 
+                  "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", 
+                  "17:00", "17:30", "18:00"]
 
 -- Função para iniciar a busca
 buscaConsultas :: String -> IO ()
@@ -13,21 +19,21 @@ buscaConsultas cpf = do
     putStrLn "1. Por todas as suas consultas"
     putStrLn "2. Por Data"
     putStrLn "3. Por Médico"
+    putStrLn "4. Ver todos os horários disponíveis"
     putStr "Digite sua escolha: "
     hFlush stdout
     escolha <- getLine
     case escolha of
         "1" -> do 
-            buscar cpf
+            buscarPorCpf cpf
             buscaConsultas cpf  
 
         "2" -> do 
             putStr "Informe a data (DD/MM/AAAA): "
             hFlush stdout
             dataConsulta <- getLine
-
             if validadorData dataConsulta
-                then buscar dataConsulta
+                then buscarPorData dataConsulta
                 else do
                     putStrLn "Data inválida"
                     buscaConsultas cpf 
@@ -37,13 +43,59 @@ buscaConsultas cpf = do
             hFlush stdout
             crmMed <- getLine
             if validadorFormatoCRM crmMed
-                then buscar crmMed
+                then buscarPorMedico crmMed
                 else do 
                     putStrLn "CRM inválido"
                     buscaConsultas cpf
 
-buscar :: String -> IO ()
-buscar val = do
-    conteudo <- readFile "consultas.txt"
-    let (linhasApagadas, dados) = partition (\linha -> val `isInfixOf` linha) (lines conteudo)
-    mapM_ putStrLn linhasApagadas
+        "4" -> verHorariosDisponiveis
+
+-- Função para buscar consultas por CPF do paciente
+buscarPorCpf :: String -> IO ()
+buscarPorCpf cpf = buscar cpf "consultas.txt"
+
+-- Função para buscar consultas por data específica
+buscarPorData :: String -> IO ()
+buscarPorData dataConsulta = buscar dataConsulta "consultas.txt"
+
+-- Função para buscar consultas por CRM do médico
+buscarPorMedico :: String -> IO ()
+buscarPorMedico crmMed = buscar crmMed "consultas.txt"
+
+-- Função genérica para buscar dados em arquivo
+buscar :: String -> FilePath -> IO ()
+buscar val filePath = do
+    conteudo <- readFile filePath
+    let (linhasEncontradas, _) = partition (\linha -> val `isInfixOf` linha) (lines conteudo)
+    if null linhasEncontradas
+        then putStrLn "Nenhuma consulta encontrada."
+        else mapM_ putStrLn linhasEncontradas
+
+-- Função para ver todos os horários disponíveis em um dia específico
+verHorariosDisponiveis :: IO ()
+verHorariosDisponiveis = do
+    putStr "Informe a data (DD/MM/AAAA): "
+    hFlush stdout
+    dataConsulta <- getLine
+    if validadorData dataConsulta
+        then do
+            conteudo <- readFile "consultas.txt"
+            let consultasNoDia = filter (\linha -> dataConsulta `isInfixOf` linha) (lines conteudo)
+            let horariosOcupados = [horario | consulta <- consultasNoDia, 
+                                              let campos = wordsWhen (=='|') consulta,
+                                              length campos > 3, 
+                                              let horario = campos !! 3]
+            let horariosDisponiveis = horariosPadrao \\ nub horariosOcupados
+            if null horariosDisponiveis
+                then putStrLn "Nenhum horário disponível."
+                else do
+                    putStrLn "Horários disponíveis:"
+                    mapM_ putStrLn horariosDisponiveis
+        else putStrLn "Data inválida, tente novamente."
+
+-- Função auxiliar para dividir uma string por um delimitador específico
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
--- code/Main.hs
+++ code/Main.hs
@@ -4,13 +4,15 @@ import System.IO (hFlush, stdout)
 import Cadastro (iniciarCadastro)
 import Login (login)
 import Consulta(submenuConsulta)
+import Agenda(buscaConsultas)
 
 main :: IO ()
 main = do
     putStrLn "\n\nMenu Principal:"
     putStrLn "1. Cadastrar Paciente ou Médico"
     putStrLn "2. Login"
-    putStrLn "3. Consultas"
+   -- putStrLn "3. Consultas"
+   -- putStrLn "4. Agenda"
     putStrLn "0. Sair"
     putStr "Digite sua escolha: "
     hFlush stdout
@@ -18,7 +20,8 @@ main = do
     case escolha of
         "1" -> iniciarCadastro >> main  -- Chama o submenu de cadastro
         "2" -> login >> main  -- Chama a função de login
-        "3" -> submenuConsulta >> main  -- Chama a função de login
+        "3" -> submenuConsulta >> main  -- Chama a função de consulta
+        "4" -> buscaConsultas "argumento" >> main  -- Chama a função de buscar consulta
         "0" -> putStrLn "Encerrando o sistema..."
         _   -> do
             putStrLn "Opção inválida. Tente novamente."

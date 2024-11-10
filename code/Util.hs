7e8f5d2d8cb637c159e2ecee7b192b2960b40a75
Gustavo Barbosa <gustavobarbosa7162@gmail.com>
2024-11-09 22:14:28 -0300

    Adiciona escolherMedico function

--- code/Util.hs
+++ code/Util.hs
@@ -1,4 +1,4 @@
-module Util (validadorCpf, validadorData, validadorFormatoCRM) where
+module Util (validadorCpf, validadorData, validadorFormatoCRM, escolherMedico) where
 
 import Text.Regex (mkRegex, matchRegex)
 import Data.Char (isDigit)
@@ -22,3 +22,43 @@ validadorFormatoCRM crm =
     in case matchRegex regex crm of
         Just _ -> True
         Nothing -> False
+
+
+-- Extrai o nome e a especialidade do médico
+medicoNomeEspecialidade :: [String] -> String
+medicoNomeEspecialidade [nome, _, _, especialidade] = nome ++ " - " ++ especialidade
+medicoNomeEspecialidade _ = "Dados incompletos"
+
+-- Função para dividir uma linha em colunas usando um delimitador
+splitLinha :: Char -> String -> [String]
+splitLinha delim linha = case break (== delim) linha of
+    (parte, "")     -> [parte]
+    (parte, _:resto) -> parte : splitLinha delim resto
+
+-- Função para ler o arquivo e mostrar a lista de médicos enumerada
+listarMedicos :: IO ()
+listarMedicos = do
+    conteudo <- readFile "medicos.txt"
+    let linhas = tail (lines conteudo)  -- Remove a primeira linha (cabeçalho)
+        medicos = map (splitLinha '|') linhas
+
+    putStrLn "\n\nEscolha um médico pelo número:"
+    mapM_ (\(i, medico) -> putStrLn (show i ++ ". " ++ medicoNomeEspecialidade medico)) (zip [1..] medicos)
+
+-- Função para escolher médico pelo número e retornar o CRM
+escolherMedico :: IO String
+escolherMedico = do
+    conteudo <- readFile "medicos.txt"
+    let linhas = tail (lines conteudo)
+        medicos = map (splitLinha '|') linhas
+    listarMedicos
+    putStrLn "Digite o número do médico desejado:"
+    escolha <- readLn
+    if escolha > 0 && escolha <= length medicos
+        then return (medicos !! (escolha - 1) !! 3)  -- Retorna o CRM
+        else return "Número inválido."
+
+
+
+
+                          

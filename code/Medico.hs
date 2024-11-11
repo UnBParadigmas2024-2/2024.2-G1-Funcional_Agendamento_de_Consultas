61099e5fe845df148172037035a21cfe7a38400f
Giu <alcantaragiubs@gmail.com>
2024-11-10 22:13:36 -0300

    ajustando ultimos detalhes

--- code/Medico.hs
+++ code/Medico.hs
@@ -9,9 +9,9 @@ submenuMedico :: String -> IO ()
 submenuMedico crm = do
     putStrLn "\nBem vindo (a)! O que deseja?"
     putStrLn "1. Visualizar meus dados"
-    putStrLn "2. Apagar minha conta"
-    putStrLn "3. Visualizar minha agenda (consultas agendadas e horários disponíveis)"
-    putStrLn "4. Sair do sistema"
+    --putStrLn "2. Apagar minha conta"
+    putStrLn "2. Visualizar minha agenda (consultas agendadas e horários disponíveis)"
+    putStrLn "3. Sair do sistema"
     putStr "Escolha uma opção: "
     hFlush stdout
     escolha <- getLine
@@ -19,15 +19,15 @@ submenuMedico crm = do
         "1" -> do
             exibirDadosMedicos crm
             submenuMedico crm  -- Retorna ao submenu após exibir os dados
-        "2" -> do
+      --  "2" -> do
             -- apagarConta crm "medicos" -- Chama a função para apagar a conta
-            putStrLn "Não implementado!"
-            submenuMedico crm
+          --  putStrLn "Não implementado!"
+          --  submenuMedico crm
 
-        "3" -> do
+        "2" -> do
             buscarConsultaMedico crm
             submenuMedico crm
-        "4" -> putStrLn "Saindo do sistema..."
+        "3" -> putStrLn "Saindo do sistema..."
         _   -> do
             putStrLn "Opção inválida. Tente novamente."
             submenuMedico crm
